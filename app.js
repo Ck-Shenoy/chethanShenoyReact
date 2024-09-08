3239253b7d82445b2741a811bc9fd5fc2d46ba2a
Chethan Kumar Shenoy <chethan.kumar@everi.com>
2024-09-08 10:57:33 +0530

    Changes

---
+++ app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const element = React.createElement("h1", {}, "Hello Chethan!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(element);
