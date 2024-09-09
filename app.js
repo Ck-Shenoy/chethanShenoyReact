76b8c52ae4ae4baefe4e85cd2e5e33d77171f208
Chethan Kumar Shenoy <chethan.kumar@everi.com>
2024-09-09 09:42:50 +0530

    react related

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-const element = React.createElement("h1", {}, "Hello Chethan!");
+let count = 0;
+const handleClick = () =>
+{
+    setButtonText('Loading...');
+
+    setTimeout(() =>
+    {
+        setButtonText('Submit');
+    }, 2000); // Reverts back to 'Submit' after 2 seconds
+};
+const Element = () => <h1>Hello I am the count is {count}</h1>;
+const Element2 = () => (
+    <div>
+        {Element()}
+        <h2>Nested Element</h2>
+        <p>This is a nested element</p>
+        <button onClick={handleClick}>submit</button>
+    </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(element);
+root.render(<Element2 />);
